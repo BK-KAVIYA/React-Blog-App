37819d42ccddec59ab4bc1c8c30f617931d00e8f
Baladurage Kavinda <63805835+BK-KAVIYA@users.noreply.github.com>
2024-03-29 09:08:10 +0530

    fetch data from json server

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -4,12 +4,7 @@ import {useState,useEffect} from 'react';
 
 const Home = () => {
 
-    const [blogs,setBlog] = useState([
-        {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
-        {title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2},
-        {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3},
-        {title: 'Web Technology', body: 'lorem ipsum...', author: 'vimudya', id: 4},
-    ]);
+    const [blogs,setBlog] = useState(null);
 
     const handleDelete = (id) => {
         console.log(id);
@@ -18,15 +13,21 @@ const Home = () => {
     }
 
     useEffect(()=>{
-        
-    });
+        fetch('http://localhost:8000/blogs')
+        .then(res=>{
+            return res.json();
+        })
+        .then((data)=>{
+            setBlog(data);
+        })
+    },[]);
 
 
     return ( 
         <div className="home">
             <h2>Home Page</h2>
             <p>Welcome to the Blog App</p>
-                <Blog blogs={blogs} title="All the Blogs.." handleDelete={handleDelete}/>
+                { blogs && <Blog blogs={blogs} title="All the Blogs.." handleDelete={handleDelete}/>}
                 {/* <Blog blogs={blogs.filter((blogs)=>blogs.author==='mario')} title="Mario's Blogs.."/>             */}
         </div>
      );
