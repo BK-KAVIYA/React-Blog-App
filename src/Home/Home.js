f6b62473b2122e299dc4b4e98a3a80aafa816d82
Baladurage Kavinda <63805835+BK-KAVIYA@users.noreply.github.com>
2024-03-29 09:37:02 +0530

    handling fetch error

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -6,16 +6,27 @@ const Home = () => {
 
     const [blogs,setBlog] = useState(null);
     const [isPending,setIsPending] = useState(true);
+    const [error,setError] = useState(null);
 
 
     useEffect(()=>{
-        fetch('http://localhost:8000/blogs')
+        fetch('http://localhost:8000/blogsS')
         .then(res=>{
-            return res.json();
+            if(!res.ok){
+                throw Error('Could not fetch the data for that resource');
+            }else{
+                return res.json();
+            }
+            
         })
         .then((data)=>{
             setBlog(data);
             setIsPending(false);
+            setError(null);
+        })
+        .catch((err)=>{
+            setIsPending(false);
+            setError(err.message);
         })
     },[]);
 
@@ -24,6 +35,7 @@ const Home = () => {
         <div className="home">
             <h2>Home Page</h2>
             <p>Welcome to the Blog App</p>
+                {error && <div>{error}</div>}
                 {isPending && <div>Loading...</div>}
                 { blogs && <Blog blogs={blogs} title="All the Blogs.."/>}
                 {/* <Blog blogs={blogs.filter((blogs)=>blogs.author==='mario')} title="Mario's Blogs.."/>             */}
